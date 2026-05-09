3f9b231ca8c1cbc425d27c3f41dab012888c9160
rarukutty <prabhakarana910@gmail.com>
2026-05-09 19:03:08 +0530

    poda lusu

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Welocome to the git course");
+// Basic JavaScript example
+console.log("Hello, World!");
+
+// Variable declaration
+const name = "JavaScript";
+const version = 2024;
+
+// Simple function
+function greet(userName) {
+    return `Hello, ${userName}!`;
+}
+
+// Function call
+console.log(greet("Developer"));
+
+// Array example
+const numbers = [1, 2, 3, 4, 5];
+console.log("Numbers:", numbers);
+
+// Object example
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+};
+
+console.log("Person:", person);
